fee5a5ce9b7f864ed468271b01b6cb874f5effc6
Zachatoo <Zachatoo@users.noreply.github.com>
2019-10-25 18:39:16 -0600

    Initial Setup

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   token: ('TOKEN' in process.env ? process.env : require('../config')).TOKEN,
-  prefix: '.',
+  prefix: '!',
   commandDelimiter: '\n',
   commandLimit: '5',
-  owners: ['206161807491072000']
+  owners: ['106998439912431616']
 };
